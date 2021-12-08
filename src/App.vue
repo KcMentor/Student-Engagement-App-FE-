b28e7d6e9d44b3e3c6da0bd583ff5d178fbbd44d
JUST0N1AN <mahabirjustin@gmail.com>
2021-12-08 00:31:10 -0400

    Color Palette

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
     <nav>
     <div class="nav-wrapper">
-      <a href="#!" class="brand-logo-left"><img :src="image" width="154" height="55"></a>
+      <a href="#!" class="brand-logo"><img :src="image" width="154" height="55" margin-left="0px"></a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
+        <li><a href="sass.html"></a></li>
         <li><a href="badges.html">Components</a></li>
         <li><a href="collapsible.html">Javascript</a></li>
         <li><a href="mobile.html">Mobile</a></li>
@@ -45,8 +45,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 0px;
+  
+}
+.nav-wrapper{
+    background-color:#350B49;
 }
 </style>
