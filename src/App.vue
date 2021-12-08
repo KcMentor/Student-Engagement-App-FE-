cab8e06058d2c3efbbc5039634962d17f5397e9c
JUST0N1AN <mahabirjustin@gmail.com>
2021-12-07 23:46:58 -0400

    Menu Icon Fixed

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
 <template>
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo-left"><img :src="image" width="154" height="55"></a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="sass.html">Sass</a></li>
+    <li><a href="badges.html">Components</a></li>
+    <li><a href="collapsible.html">Javascript</a></li>
+    <li><a href="mobile.html">Mobile</a></li>
+  </ul>
   <HomePage/>
 </template>
 
 <script>
 import HomePage from './components/HomePage.vue'
+import image from "./assets/AppLogo.png"
+
 
 export default {
+
   name: 'App',
+  data: function(){
+    return{
+      image: image
+    }
+  },
   components: {
     HomePage
   }
