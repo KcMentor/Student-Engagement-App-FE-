9f2f0c939cf3720181a96462c4a7253aff07a031
JUST0N1AN <mahabirjustin@gmail.com>
2021-12-14 17:16:19 -0400

    Added Firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDYipVLwE-yJmQ0wRgW8Gl6BksV0j4uyYM",
+    authDomain: "engaged-app.firebaseapp.com",
+    projectId: "engaged-app",
+    storageBucket: "engaged-app.appspot.com",
+    messagingSenderId: "155303329273",
+    appId: "1:155303329273:web:167b05c1ebc68be924d128",
+    measurementId: "G-BJ8J63WJTB"
+};
+
+firebase.initializeApp(firebaseConfig)
+
 
 createApp(App).use(router).mount('#app')
