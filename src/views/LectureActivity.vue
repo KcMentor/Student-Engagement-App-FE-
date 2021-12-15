cf0e5597f377375a4c020dc3eef3483824ea359e
Kc Mentor-Alexis <kc.mentoralexis@my.uwi.edu>
2021-12-15 04:14:23 -0400

    Updated Icon

--- src/views/LectureActivity.vue
+++ src/views/LectureActivity.vue
@@ -1,10 +1,22 @@
 <template>
-   <div class="row">
+  <div class="row">
     <div class="col s12 m4">
       <div class="card my-color-card">
         <div class="card-content my-color">
-            <i class="large material-icons my-color">add_circle_outline</i>   
-            <h4>Create Question Bank </h4>
+          <a href="#modal">
+            <i class="large material-icons my-color">add_circle_outline</i>
+          </a>
+          <div id="modal1" class="modal">
+            <div class="modal-content">
+              <h4>Modal Header</h4>
+              <p>A bunch of text</p>
+            </div>
+            <div class="modal-footer">
+              <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+            </div>
+          </div>
+
+          <h4>Create Question Bank</h4>
         </div>
       </div>
     </div>
@@ -13,21 +25,17 @@
 
 <script>
 export default {
-    
-
 }
 </script>
 
 <style>
-  .my-color{
-    color: #4E2D68
-  }
-  .my-color-card{
-    background-color:#EDE7F0
-  }
-  .card-content{
-    text-align: center !important
-
-  }
-
+.my-color {
+  color: #4e2d68;
+}
+.my-color-card {
+  background-color: #ede7f0;
+}
+.card-content {
+  text-align: center !important;
+}
 </style>
