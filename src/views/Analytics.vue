9156269c3b472154df68a3dadc38a0221bb3ce15
makaelawallace <78979705+makaelawallace@users.noreply.github.com>
2021-12-15 17:28:34 -0400

    Lecturer Home Screen

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -1,7 +1,9 @@
-<template>
 
-    <div class=" card-panel my-color-card wrapper">
-        <table class="highlight">
+
+<template>
+   
+     <div class=" card-panel my-color-card wrapper">
+        <table class="highlight centered responsive-table">
             <thead>
             <tr>
                 <th>Sessions</th>
@@ -10,22 +12,29 @@
 
             <tbody>
             <tr>
-                <td>COMP 3613 Lecture 1
-                    <br>
-                    <br>
-                </td>
+                <td>COMP 3613 Lecture 1</td>
             </tr>
             <tr>
-                <td>COMP 3613 Lecture 2
-                    <br>
-                    <br>
-                </td>
+                <td>COMP 3613 Lecture 2</td>
             </tr>
             <tr>
-                <td>COMP 3613 Lecture 3
-                    <br>
-                    <br>
-                </td>
+                <td>COMP 3613 Lecture 3</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class=" card-panel my-color-card wrapper2">
+        <table class="highlight centered responsive-table">
+            <thead>
+            <tr>
+                <th>Students</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            <tr>
+                <td>N/A</td>
             </tr>
             </tbody>
         </table>
@@ -35,21 +44,94 @@
 <script>
 // @ is an alias to /src
 
-export default {
-  name: "Analytics",
-  components: {},
-};
+
+    export default {
+        name: "Analytics",
+        
+        components : {
+        }
+    }
+
+
 </script>
 
 <style>
 
 .wrapper{
-    width: 700px;
-    height: 300px;
+    width: 600px;
+    height: 250px;
     border: 1px solid #c3c3c3;
-    display: flex;
-    flex-wrap: wrap;
-    align-content: center;
+    position: absolute;
+    bottom: 0px; 
+    right:15px;
+    color: #4E2D68;
+}
+
+.wrapper2{
+    width: 600px;
+    height: 200px;
+    border: 1px solid #c3c3c3;
+    position: absolute;
+    bottom: 0px; 
+    left: 15px;
+    color: #4E2D68;
+}
+
+.carousel-item {
+    display:flex;
+    justify-content: center;
+}
+
+.carousel {
+    position:relative;
+    overflow: hidden;
+    width:800px;
+    height:500px;
+    z-index:10;
+}
+    
+.btn {
+    padding:5px 10px;
+    background-color:rgba(0,0,0,0.5);
+    border:1px solid transparent;
+    margin:5px 10px;
+    color:#FFF;
+    height:50px;
+    width:50px;
+    position:absolute;
+    margin-top:-25px;
+    z-index:2;
+}
+    
+.btn:hover {
+    cursor: pointer;
+}
+    
+.btn:focus{
+    outline:none;
+}
+    
+.btn-next {
+    top:50%;
+    right:0;
+}
+    
+.btn-prev {
+    top:50%;
+    left:0;
+}
+    
+.carousel-slider {
+    position:absolute;
+    top:0;
+    left:0;
+    bottom:0;
+    right:0;
+}
+    
+.carousel-slider img {
+    width:100%;
+    height:100%;
 }
 
 </style>
--- src/views/LectureHome.vue
+++ src/views/LectureHome.vue
@@ -3,6 +3,45 @@
     <h1>Lecture Home</h1>
     <router-link to="/lecturerActivity"><a>View Activities</a></router-link>
   </div>
+
+  <div class="container center-align selection" style="background-color:#d1c4e9;width:65vw;">
+   <div class="row option" >
+      <div class="col s12 " >
+          <table class="highlight  responsive-table">
+              <thead>
+              <tr>
+                  <th>Sessions</th>
+              </tr>
+              </thead>
+
+              <tbody>
+              <tr>
+                  <td><b>COMP 3602 - Theory of Computing</b>
+                  <br> 10:00am - 12:00pm</td>
+              </tr>
+              <tr>
+                  <td><b>COMP 3611 - Data Structures</b>
+                    <br> 12:00pm - 1:00pm</td>
+              </tr>
+              <tr>
+                  <td><b>COMP 3605 - Data Analytics</b>
+                    <br> 1:00pm - 2:00pm</td>
+              </tr>
+              </tbody>
+          </table>
+          
+      </div>
+
+      <a class="waves-effect waves-light btn-large deep-purple lighten-1">Monday</a>
+      <a class="waves-effect waves-light btn-large deep-purple lighten-1">Tuesday</a>
+      <a class="waves-effect waves-light btn-large deep-purple lighten-1">Wednesday</a>
+      <a class="waves-effect waves-light btn-large deep-purple lighten-1">Thursday</a>
+      <a class="waves-effect waves-light btn-large deep-purple lighten-1">Friday</a>
+      
+    </div>
+
+   
+  </div>
 </template>
 
 <script>
@@ -13,3 +52,19 @@ export default {
   components: {},
 };
 </script>
+
+<style>
+  .container{
+    position: relative;
+    color:  #4E2D68;
+  }
+
+  table{
+    width: 400px;
+  }
+
+  .btn-large{
+    border-radius: 12px;
+  }
+
+</style>
