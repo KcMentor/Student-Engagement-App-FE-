a9c1ed042165e417893b6c3e50fcbb2f9fc8bd92
makaelawallace <78979705+makaelawallace@users.noreply.github.com>
2021-12-15 21:52:11 -0400

    Lecture Session Screen

--- src/views/LectureSession.vue
+++ src/views/LectureSession.vue
@@ -1,4 +1,13 @@
 <template>
+
+   <form role="search" id="form">
+  <input type="search" id="query" name="q"
+   placeholder="Search..."
+   aria-label="Search through site content">
+  <button>
+    <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
+  </button>
+</form>
     
     <div class="cards">
         <div class="row">
@@ -21,8 +30,13 @@
             <a href="#modal">
              <i class="material-icons my-color right">clear</i>
             </a>
-          <span class="card-title"> <b>COMP 3607</b></span>
-          <p>10:00AM - 11:00AM</p>
+          <span class="card-title center"> <b><br>Theory of Computing</b>
+              <br>COMP 3602</span>
+               <div class="card-action #ede7f0">
+                <p>10:00AM - 11:00AM
+                <br>15th Jan, 2022
+          </p>
+        </div>
         </div>
       </div>
     </div>
@@ -47,10 +61,56 @@ export default {
     }
 
     .cards{
+        top: 150px;
         height: 250px;
         position: absolute;
         display: flex;
         flex-direction: row;
         row-gap: 10px;
     }
+
+    form {
+        background-color: #ede7f0;
+        position: relative;
+        width: 300px;
+        height: 44px;
+        left: 20px;
+        top: 20px;
+        border-radius: 5px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+    input {
+        all: unset;
+        font: 16px system-ui;
+        color: gray;
+        height: 100%;
+        width: 100%;
+        padding: 6px 10px;
+    }
+
+    ::placeholder {
+        color: gray;
+        opacity: 0.7; 
+        position: relative;
+        left: 5px;
+    }
+
+    button {
+        all: unset;
+        cursor: pointer;
+        width: 44px;
+        height: 44px;
+    }
+
+    svg {
+        color: gray;
+        fill: currentColor;
+        width: 24px;
+        height: 24px;
+        padding: 10px;
+    }
+    
+
 </style>
